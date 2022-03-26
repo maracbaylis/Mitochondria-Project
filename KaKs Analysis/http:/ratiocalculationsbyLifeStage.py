9b06ed2c4f98346e78cf4aefe97e06d111a5104b
maracbaylis <88911118+maracbaylis@users.noreply.github.com>
2022-03-25 17:00:29 -0700

    Create ratiocalculationsbyLifeStage.py

---
+++ KaKs Analysis/http:/ratiocalculationsbyLifeStage.py
@@ -0,0 +1,8 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+from tqdm.notebook import tqdm
+import math
+from scipy.stats import hypergeom
+from scipy.stats import fisher_exact
